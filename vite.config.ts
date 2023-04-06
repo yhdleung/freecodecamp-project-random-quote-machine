34f3bf0a9936f34a1fd13861b3e4b723843f87c2
Dick Leung <yhd.leung@gmail.com>
2023-04-06 15:26:46 +0800

    update Vite project config to deploy to Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://yhdleung.github.io/freecodecamp-project-random-quote-machine/",
   plugins: [react()],
 })
